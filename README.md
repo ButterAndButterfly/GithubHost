4b8d0f2749134343882ce247a07e546be69e274b
root <lijia0732@sina.com>
2020-05-23 11:45:30 +0800

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# GithubHost
+## 前言  
+因为有几个域名被DNS污染了，GitHub老是看不见图，遂有了这个工程。虽然没大用是了。。。
+
+## 功能  
+利用海外的机器进行相关网站的DNS查询, 将得到的结果发布到issue页面。  
++ 支持周期性自动发布
+    + 每月1/16号发布issue
++ 支持自动回复  
+    + 如果想查询最新的host，可以自己开个issue，自动回复。
+    + 举例[issue #1](https://github.com/nICEnnnnnnnLee/GithubHost/issues/1)
+
+## 查看
++ 请移步[issue](https://github.com/nICEnnnnnnnLee/GithubHost/issues/)页面。   
++ 更改hosts后，注意`ipconfig /flushdns`刷新DNS缓存。
+
---
+++ socket_query.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding:utf-8
+ 
+import socket
+
+def output_hosts():
+    domains = ['github.com',
+                'gist.github.com',
+                'assets-cdn.github.com',
+                'raw.githubusercontent.com',
+                'gist.githubusercontent.com',
+                'cloud.githubusercontent.com',
+                'camo.githubusercontent.com',
+                'avatars0.githubusercontent.com',
+                'avatars0.githubusercontent.com',
+                'avatars1.githubusercontent.com',
+                'avatars1.githubusercontent.com',
+                'avatars2.githubusercontent.com',
+                'avatars2.githubusercontent.com',
+                'avatars3.githubusercontent.com',
+                'avatars3.githubusercontent.com',
+                'avatars4.githubusercontent.com',
+                'avatars4.githubusercontent.com',
+                'avatars5.githubusercontent.com',
+                'avatars5.githubusercontent.com',
+                'avatars6.githubusercontent.com',
+                'avatars6.githubusercontent.com',
+                'avatars7.githubusercontent.com',
+                'avatars7.githubusercontent.com',
+                'avatars8.githubusercontent.com',
+                'avatars8.githubusercontent.com',
+                'api.github.com']
+    
+    with open('hosts.txt', 'w') as f:
+        f.write('```\n')
+        f.write('# GitHub Start \n')
+        for domain in domains:
+            print('Querying ip for domain %s'%domain)
+            ip = socket.gethostbyname(domain)
+            print(ip)
+            f.write('%s %s\n'%(ip, domain))
+        f.write('# GitHub End \n') 
+        f.write('```\n')
+        
+        
+    
+if __name__ == '__main__':
+    output_hosts()
